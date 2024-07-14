e6a3f51c3e7e6added464d2c661f91b9d888dc78
NelsonFrank <nelsonfr78@outlook.com>
2024-07-15 01:43:49 +0300

    feat: :sparkles: add swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,19 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Shopkeeper Backend App')
+    .setDescription('The backend RESTFul API for shopkeeper app and webapp')
+    .setVersion('1.0')
+    .addTag('commerce')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
